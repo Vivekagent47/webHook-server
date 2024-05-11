8f54f0b6bca027cb0f709ef7a58a807e2351268c
Vivek Chauhan <vivek.agent47@gmail.com>
2024-05-11 21:13:33 +0530

    fix: nanoid fix

--- src/utils/help.ts
+++ src/utils/help.ts
@@ -1,3 +1,6 @@
+// Right now using nanoid@3 because from nanoid@4 ESM is not supported in the nanoid
+// build code by Nestjs is in ESM format, which is breaking the build
+// So, we are using nanoid@3 for now
 import { customAlphabet } from "nanoid";
 
 const alphabet =
